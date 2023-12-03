afd6fb04bec7a0b3d458fb413280b86e9e17c3b3
thetalemon <sasami0314@gmail.com>
2023-12-03 21:02:18 +0900

    novelのdetaを切り出し

---
+++ src/data/novel.ts
@@ -0,0 +1,15 @@
+export const NOVEL_LIST = [
+  {
+    title: "水のたまゆら",
+    stories: [
+      {
+        title: "瀬知と司ととある雨の日",
+        href: "/novel/tamayura/rainy",
+      },
+      {
+        title: "はじまり",
+        href: "/novel/tamayura/abstract",
+      },
+    ],
+  },
+];
