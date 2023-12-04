e8670d9bef5b9c6eda6f54a34f4884b588f8b964
thetalemon <sasami0314@gmail.com>
2023-12-05 00:16:18 +0900

    style生成用のhelperを作成

---
+++ src/utils/styleHelper.ts
@@ -0,0 +1,12 @@
+type StyleHelperProps = {
+  name: string;
+  value: string;
+};
+
+export const styleHelper = (array: StyleHelperProps[]) => {
+  return array.reduce((acc, cur) => {
+    const { name, value } = cur;
+    const tmp = `--${name}: ${value};`;
+    return `${acc} ${tmp}`;
+  }, "");
+};
