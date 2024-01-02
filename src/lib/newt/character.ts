28071e00eaff255703a7d436faead681f954c232
thetalemon <sasami0314@gmail.com>
2024-01-02 16:06:55 +0900

    キャラ紹介を追加

---
+++ src/lib/newt/character.ts
@@ -0,0 +1,28 @@
+import { newtClient, APP_UID } from "./general";
+import type { NewtImage } from "./general";
+import type { SeriesItem } from "./series";
+
+interface CharacterItem {
+  name: string;
+  slug: string;
+  image: NewtImage;
+  content: string;
+  series: SeriesItem;
+}
+
+const MODEL_UID = "character";
+const UIDS = {
+  appUid: APP_UID,
+  modelUid: MODEL_UID,
+};
+
+export const getCharacterList = async (): Promise<CharacterItem[]> => {
+  const { items: novels } = await newtClient.getContents<CharacterItem>({
+    ...UIDS,
+    query: {
+      select: ["slug", "name", "content", "image", "series"],
+      order: ["-_sys.customOrder"],
+    },
+  });
+  return novels;
+};
