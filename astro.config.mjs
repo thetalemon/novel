620c48d175ee1cf05d148209f72ce8ba08c8d227
thetalemon <sasami0314@gmail.com>
2024-01-07 23:22:57 +0900

    ogImageの追加

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   image: {
-    domains: ["manas-novel.assets.newt.so"],
+    domains: ["manas-novel.assets.newt.so", "novel.manasas.dev"],
   },
   site: "https://novel.manasas.dev",
+  integrations: [react()],
+  vite: { optimizeDeps: { exclude: ["@resvg/resvg-js"] } },
 });
---
+++ src/pages/novel/og/[slug].png.ts
@@ -0,0 +1,23 @@
+import type { APIContext } from "astro";
+import { getHistoryList } from "@/lib/newt/notify";
+import { getOgImage } from "@/components/OgImage";
+
+export async function getStaticPaths() {
+  const hisotryList = await getHistoryList();
+
+  return hisotryList.map((item) => ({
+    params: { slug: item.slug },
+    props: { item },
+  }));
+}
+
+export async function GET({ props }: APIContext) {
+  console.log(props.title);
+  const body = await getOgImage(props.item.title ?? "No title");
+
+  return new Response(body, {
+    headers: {
+      "content-type": "image/png",
+    },
+  });
+}
