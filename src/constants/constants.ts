0b3e39b771d6f5d07482cf21cbfb3ea9666942d2
thetalemon <sasami0314@gmail.com>
2024-09-22 18:24:16 +0900

    package update and 少しリファクタ

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -3,5 +3,5 @@ export const myUrl = `https://${myDomain}`
 export const myMainUrl = `https://manasas.dev`
 export const myName = 'manasas'
 export const mySiteName = '北極の とある倉庫'
-export const mySiteDefaultDescription = 'まなさすの創作サイト'
+export const mySiteDefaultDescription = 'まなさすの創作保管庫'
 export const myCopyLight = `© 2023 ${myName}`
