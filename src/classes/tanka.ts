1bca248306cb961305233b274895ed32b474a365
thetalemon <sasami0314@gmail.com>
2024-09-24 22:54:18 +0900

    refactor: コンポーネントを分割 & package update

---
+++ src/classes/tanka.ts
@@ -0,0 +1,5 @@
+export const jsTankaMain = 'js-tankaMain'
+
+// ここに定義するクラス名たちを
+// className.get()ならクラス名(className)
+// className.getSelector()ならセレクタ名（.className）をとれるようにしたい
---
+++ src/types/tanka.d.ts
@@ -0,0 +1,4 @@
+export type Tanka = {
+  first: string
+  second: string
+}
