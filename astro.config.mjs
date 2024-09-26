9a04a4c3e41f454b1a2a6d9473475a446640951b
thetalemon <sasami0314@gmail.com>
2024-09-26 22:40:54 +0900

    sitemapをいれてみる

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,14 @@ import react from '@astrojs/react'
 import icon from 'astro-icon'
 import { defineConfig } from 'astro/config'
 
+import sitemap from '@astrojs/sitemap'
+
 export default defineConfig({
   image: {
     domains: ['manas-novel.assets.newt.so', 'novel.manasas.dev'],
   },
   site: 'https://novel.manasas.dev',
-  integrations: [react(), icon()],
+  integrations: [react(), icon(), sitemap()],
   vite: {
     optimizeDeps: {
       exclude: ['@resvg/resvg-js'],
