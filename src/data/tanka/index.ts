590c6d07ae715a42eed15d4a0d08ff56fee23389
thetalemon <sasami0314@gmail.com>
2024-10-06 20:24:26 +0900

    短歌

--- src/data/tanka/index.ts
+++ src/data/tanka/index.ts
@@ -1,3 +1,5 @@
 import { summer2024 } from '@/data/tanka/pages/2024summer'
+import { q1of2024 } from '@/data/tanka/pages/2024Q1'
+import { q2of2024 } from '@/data/tanka/pages/2024Q2'
 
-export const allTankaList = [summer2024]
+export const allTankaList = [summer2024, q1of2024, q2of2024]
