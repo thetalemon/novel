e800e4d595aadd96a163aab872e462d6e74fb316
thetalemon <sasami0314@gmail.com>
2024-10-14 01:36:36 +0900

    同盟リンク等を追加

---
+++ src/data/link/doumei.ts
@@ -0,0 +1,147 @@
+import Sousekai from '@/img/banners/sousekai.gif'
+import ReiwaKojinSite from '@/img/banners/reiwakojinsite.png'
+import Jimoe from '@/img/banners/jimoe.gif'
+import NotSayukotei from '@/img/banners/notSayukotei.gif'
+import Heki from '@/img/banners/heki.gif'
+import Kaku from '@/img/banners/kaku.png'
+import Onigiri from '@/img/banners/onigiri.gif'
+import SiteSuki from '@/img/banners/sitesuki.png'
+import Nami from '@/img/banners/nami.gif'
+import Zatu from '@/img/banners/zatu.gif'
+import Chihitu from '@/img/banners/chihitu.gif'
+import Maste from '@/img/banners/maste.png'
+import Koujyoshin from '@/img/banners/koujyoshin.png'
+import Zassyoku from '@/img/banners/zassyoku.gif'
+import Ahiru from '@/img/banners/ahiru.png'
+import Seiheki from '@/img/banners/seiheki.png'
+import JiSaitoAi from '@/img/banners/jisaitoai.png'
+import SiteCustom from '@/img/banners/sitecustom.png'
+
+export const DoumeiLinks = [
+  {
+    href: 'https://flanet.web.fc2.com/union/site_enjoy/',
+    src: ReiwaKojinSite,
+    width: 120,
+    height: 60,
+    alt: '令和も個人サイト楽しみ隊！',
+  },
+  {
+    href: 'https://unionmatome.web.fc2.com/jimoe/index.html',
+    src: Jimoe,
+    width: 140,
+    height: 32,
+    alt: 'メイドイン自萌え',
+  },
+  {
+    href: 'https://emd.lsv.jp/yuru-union/',
+    src: JiSaitoAi,
+    width: 88,
+    height: 31,
+    alt: '自サイト愛',
+  },
+  {
+    href: 'https://gedounohakarai.lsv.jp/doumei/sitecustom-omoroi',
+    src: SiteCustom,
+    width: 88,
+    height: 31,
+    alt: 'サイトカスタムおもろい！',
+  },
+  {
+    href: 'https://lucepastorale.web.fc2.com/unione/sousekai.html',
+    src: Sousekai,
+    width: 88,
+    height: 31,
+    alt: '創造世界同盟',
+  },
+  {
+    href: 'https://healchan.neocities.org/kaku/',
+    src: Kaku,
+    width: 88,
+    height: 31,
+    alt: '書く。同盟',
+  },
+  {
+    href: 'https://mastekan.sakura.ne.jp/union/',
+    src: Maste,
+    width: 88,
+    height: 31,
+    alt: 'マステLOVE同盟',
+  },
+  {
+    href: 'https://unionmatome.web.fc2.com/riba/index.html',
+    src: NotSayukotei,
+    width: 88,
+    height: 31,
+    alt: '左右固定していません',
+  },
+  {
+    href: 'https://emd.lsv.jp/yuru-union/',
+    src: Seiheki,
+    width: 88,
+    height: 31,
+    alt: '性癖に正直なサイト',
+  },
+  {
+    href: 'https://unionmatome.web.fc2.com/jikoman/index.html',
+    src: Heki,
+    width: 110,
+    height: 20,
+    alt: '自己満足で運営中。',
+  },
+  {
+    href: 'https://lyze.jp/zenbu/',
+    src: Zassyoku,
+    width: 95,
+    height: 20,
+    alt: '雑食同盟',
+  },
+  {
+    href: 'https://holydragoon.jp/site_love_union',
+    src: SiteSuki,
+    width: 40,
+    height: 40,
+    alt: 'サイト好き同士の会',
+  },
+  {
+    href: 'https://osoiyo.ma-jide.com/',
+    src: Chihitu,
+    width: 32,
+    height: 32,
+    alt: '遅筆同盟',
+  },
+  {
+    href: 'https://rs.skr.jp/aspiration/',
+    src: Koujyoshin,
+    width: 32,
+    height: 32,
+    alt: '向上心',
+  },
+  {
+    href: 'https://sozaikuzu.blogspot.com/p/doumei.html',
+    src: Ahiru,
+    width: 32,
+    height: 32,
+    alt: 'ゴムのアヒルちゃん',
+  },
+  {
+    href: 'https://zattaratta.chottu.net/',
+    src: Zatu,
+    width: 30,
+    height: 30,
+    alt: '雑多サイト同盟',
+  },
+  {
+    href: 'https://lyze.jp/namiup/fp/3/',
+    src: Nami,
+    width: 23,
+    height: 23,
+    alt: 'うちは波更新です。',
+  },
+  {
+    href: 'https://onigiru.wixsite.com/onigiri',
+    src: Onigiri,
+    width: 32,
+    height: 32,
+    alt: 'おにぎり同盟',
+  },
+]
---
+++ src/data/link/search.ts
@@ -0,0 +1,27 @@
+import Lony from '@/img/banners/lony.png'
+import Compass from '@/img/banners/compass.png'
+import Echoes from '@/img/banners/echoes.png'
+
+export const SearchLinks = [
+  {
+    href: 'https://lony.jp',
+    src: Lony,
+    width: 200,
+    height: 40,
+    alt: '創作サイト総合サーチ Lony',
+  },
+  {
+    href: 'https://compslink.jp',
+    src: Compass,
+    width: 200,
+    height: 40,
+    alt: 'COMPASS LINK',
+  },
+  {
+    href: 'https://echoes.o0o0.jp/',
+    src: Echoes,
+    width: 200,
+    height: 40,
+    alt: 'ECHOES.',
+  },
+]
