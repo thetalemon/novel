6b67ddc696cc657b8b4da2d4d79e1546e6c3e658
thetalemon <sasami0314@gmail.com>
2025-05-06 17:46:24 +0900

    RSSのビルドが通るように

--- src/pages/feed.xml.ts
+++ src/pages/feed.xml.ts
@@ -3,31 +3,31 @@ import {
   mySiteName,
   myUrl,
 } from '@/constants/constants'
-import rss from '@astrojs/rss'
+import rss, { type RSSFeedItem } from '@astrojs/rss'
 import type { APIContext } from 'astro'
 import { HistoryList } from '@/data/history'
 
 export async function GET(context: APIContext) {
   const posts = HistoryList
   const baseUrl = myUrl
+
+  const items: RSSFeedItem[] = posts.map((post) => {
+    const url = `${baseUrl}/notify/${post.date}`
+    const dateStr = `${post.date.slice(0, 4)}-${post.date.slice(4, 6)}-${post.date.slice(6, 8)}`
+
+    return {
+      title: post.title,
+      description: `<img src="https://novel.manasas.dev/notify/og/${post.date}.png" alt="${post.title}"/> ${post.description}`,
+      link: url,
+      pubDate: new Date(dateStr),
+    }
+  })
+
   return rss({
     title: mySiteName,
     description: mySiteDefaultDescription,
     site: context.site ?? myUrl,
-    items: posts.map((post) => {
-      const url = `${baseUrl}/notify/${post.date}`
-      return {
-        title: post.title,
-        description: `<img src="https://novel.manasas.dev/notify/og/${post.date}.png"/> ${post.description}`,
-        content: post.description,
-        id: url,
-        link: url,
-        pubDate: new Date(post.date),
-        enslosure: {
-          url: `https://novel.manasas.dev/notify/og/${post.date}.png`,
-        },
-      }
-    }),
+    items,
     customData: `<language>ja</language>`,
   })
 }
