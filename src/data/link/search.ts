3152e2c54e6a6fe78d2b1cc76d5717e2bf8712ab
thetalemon <sasami0314@gmail.com>
2025-11-06 16:29:19 +0900

    ENTERページを追加　ついでに諸々断捨離

--- src/data/link/search.ts
+++ src/data/link/search.ts
@@ -1,16 +1,8 @@
-import Lony from '@/img/banners/lony.png'
 import Compass from '@/img/banners/compass.png'
 import Echoes from '@/img/banners/echoes.png'
 import type { LinkImage } from '@/types/link'
 
 export const SearchLinks: LinkImage[] = [
-  {
-    href: 'https://lony.jp',
-    src: Lony,
-    width: 200,
-    height: 40,
-    alt: '創作サイト総合サーチ Lony',
-  },
   {
     href: 'https://compslink.jp',
     src: Compass,
