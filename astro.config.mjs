1519bf9e8232b16bf692e6fa4a71300bf9e24556
thetalemon <sasami0314@gmail.com>
2026-04-27 21:37:55 +0900

    package update

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,14 @@ import icon from 'astro-icon'
 import { defineConfig } from 'astro/config'
 import sitemap from '@astrojs/sitemap'
 
+import mdx from '@astrojs/mdx';
+
 export default defineConfig({
   image: {
     domains: ['novel.manasas.dev'],
   },
   site: 'https://novel.manasas.dev',
-  integrations: [icon(), sitemap()],
+  integrations: [icon(), sitemap(), mdx()],
   vite: {
     css: {
       preprocessorOptions: {
