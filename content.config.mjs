e0b4e09c02afd7a26ab06a9ab5475b0d6d2405b2
thetalemon <sasami0314@gmail.com>
2026-05-10 22:56:48 +0900

    がっつり改装

---
+++ content.config.mjs
@@ -0,0 +1,6 @@
+import { defineCollection } from 'astro:content'
+
+export const collections = {
+  blog: defineCollection({}),
+  novel: defineCollection({}),
+}
---
+++ src/content.config.ts
@@ -0,0 +1,23 @@
+import { defineCollection } from 'astro:content'
+import { z } from 'zod'
+import { glob } from 'astro/loaders'
+
+const blog = defineCollection({
+  loader: glob({ pattern: '**/[^_]*.{md,mdx}', base: './src/content/blog' }),
+  schema: z.object({
+    title: z.string(),
+    pubDate: z.coerce.date().optional(),
+    tags: z.array(z.string()).optional(),
+  }),
+})
+
+const novel = defineCollection({
+  loader: glob({ pattern: '**/[^_]*.{md,mdx}', base: './src/content/novel' }),
+  schema: z.object({
+    title: z.string(),
+    ogImage: z.string().optional(),
+    image: z.string().optional(),
+  }),
+})
+
+export const collections = { blog, novel }
