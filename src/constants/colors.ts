ad063eb538bcb6c87e8369eeef036e34240862a2
thetalemon <sasami0314@gmail.com>
2026-07-20 22:32:23 +0900

    注意モーダルを追加しました

--- src/constants/colors.ts
+++ src/constants/colors.ts
@@ -1,16 +1,16 @@
 export const COLORS_LIST = [
   {
     slug: 'light',
-    label: '昼',
     bgColor: '223, 229, 247',
     textColor: '41, 59, 77',
     linkColor: '0, 132, 107',
+    buttonColor: '122, 65, 113',
   },
   {
     slug: 'dark',
-    label: '夜',
     bgColor: '41, 59, 77',
     textColor: '187, 200, 230',
     linkColor: '156, 200, 153',
+    buttonColor: '67, 103, 107',
   },
 ]
